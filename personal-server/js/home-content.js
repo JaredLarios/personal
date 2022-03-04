562e0871b97cd3b6df1a2e06475fa7f79fad835c
Jared Larios <lar21038@byui.edu>
2022-03-03 23:19:35 -0600

    chore(header): add img

--- personal-server/js/home-content.js
+++ personal-server/js/home-content.js
@@ -3,7 +3,12 @@ const head = document.querySelector('.init');
 
 const home = {
     header: (e) => {
-        head.innerHTML = `<h1>${e.content.header}<h1>`
+        head.innerHTML = `
+            <img src='${e.images.header}'>
+            <div class='img-header'>
+                <h1>${e.content.header}<h1>
+            </div>              
+        `
         console.log(head);
     }
 }
