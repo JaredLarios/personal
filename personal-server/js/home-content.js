5317e99314de6161bda0f9bb5c64b161b827c831
Jared Larios <lar21038@byui.edu>
2022-03-03 23:54:31 -0600

    chore(styel): add style

--- personal-server/js/home-content.js
+++ personal-server/js/home-content.js
@@ -4,7 +4,9 @@ const head = document.querySelector('.init');
 const home = {
     header: (e) => {
         head.innerHTML = `
-            <img src='${e.images.header}'>
+            <div class='img-header'>
+                <img src='${e.images.header}' alt='xbox'>
+            </div> 
             <div class='banner-header'>
                 <h1>${e.content.header}<h1>
             </div>              
